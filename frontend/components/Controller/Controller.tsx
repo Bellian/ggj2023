c08e72cee303ae5bd069b64ec5168e41ead976b1
Noel <schenknoel@gmail.com>
2023-02-05 02:12:27 +0100

    added keyboard and controller

--- frontend/components/Controller/Controller.tsx
+++ frontend/components/Controller/Controller.tsx
@@ -7,6 +7,8 @@ interface ControllerProps {}
 
 const Controller: FC<ControllerProps> = () => {
   useEffect(() => {
+    doKeyboardStuff();
+
     const gamepadconnectedEvent = betterAddEventListener(
       'gamepadconnected',
       (e) => {
@@ -56,7 +58,49 @@ function doControllerStuff() {
         controllerState.axes[2],
         controllerState.axes[3],
       ]);
-    controllerState.buttons &&
-      ControllerStore.setXButton(controllerState.buttons[0].pressed);
+    controllerState.buttons && controllerState.buttons[0].pressed
+      ? ControllerStore.setInput('x')
+      : ControllerStore.removeInput('x');
   }, 2000) as any;
+}
+
+function doKeyboardStuff() {
+  betterAddEventListener('keydown', (event) => {
+    switch (event.code) {
+      case 'KeyW':
+        ControllerStore.setInput('up');
+        break;
+      case 'KeyD':
+        ControllerStore.setInput('right');
+        break;
+      case 'KeyS':
+        ControllerStore.setInput('down');
+        break;
+      case 'KeyA':
+        ControllerStore.setInput('left');
+        break;
+      case 'Space':
+        ControllerStore.setInput('x');
+        break;
+    }
+  });
+  betterAddEventListener('keyup', (event) => {
+    switch (event.code) {
+      case 'KeyW':
+        ControllerStore.removeInput('up');
+        break;
+      case 'KeyD':
+        ControllerStore.removeInput('right');
+        break;
+      case 'KeyS':
+        ControllerStore.removeInput('down');
+        break;
+      case 'KeyA':
+        ControllerStore.removeInput('left');
+        break;
+      case 'Space':
+        ControllerStore.removeInput('x');
+        break;
+    }
+  });
 }
--- frontend/stores/controllerStore.ts
+++ frontend/stores/controllerStore.ts
@@ -5,16 +5,16 @@ import { createContext } from 'react';
 export interface IControllerStore {
   up: any;
   right: any;
-  bottom: any;
+  down: any;
   left: any;
+  x: any;
 }
 
 export class ControllerStoreClass {
   leftStick: vec2 = [0, 0];
   rightStick: vec2 = [0, 0];
-  xButton = false;
 
-  input: Set<keyof IControllerStore>;
+  input = new Set<keyof IControllerStore>();
 
   constructor() {
     makeAutoObservable(this);
@@ -24,24 +24,26 @@ export class ControllerStoreClass {
     this.input.add(input);
   }
 
+  removeInput(input: keyof IControllerStore) {
+    this.input.delete(input);
+  }
+
   // 1.- = links + = rechts
   // 2. - = oben + = unten
   setLeftStick(input: vec2) {
     this.leftStick = input;
-    0 > input[0] && this.setInput('left');
-    0 < input[0] && this.setInput('right');
+    0 > input[0] && (this.setInput('left'), this.removeInput('right'));
+    0 < input[0] && (this.setInput('right'), this.removeInput('left'));
+    0 === input[0] && (this.removeInput('left'), this.removeInput('right'));
 
-    0 > input[0] && this.setInput('up');
-    0 > input[0] && this.setInput('bottom');
+    0 > input[1] && (this.setInput('up'), this.removeInput('down'));
+    0 > input[1] && (this.setInput('down'), this.removeInput('up'));
+    0 === input[1] && (this.removeInput('up'), this.removeInput('down'));
   }
 
   setRightStick(input: vec2) {
     this.rightStick = input;
     // ¯\_(ツ)_/¯
-  }
-
-  setXButton(input: boolean) {
-    this.xButton = input;
   }
 }
 
