d681395148cdea409d7c99ed8e7f0611ffd93b61
Noel <schenknoel@gmail.com>
2023-02-05 14:26:27 +0100

    added small map

---
+++ frontend/services/levels/small.data.ts
@@ -0,0 +1,843 @@
+export default {
+  levels: [
+    [
+      {
+        name: 'factory',
+        position: 101,
+      },
+      {
+        name: 'factory',
+        position: 101,
+      },
+      {
+        name: 'factory',
+        position: 4,
+      },
+      {
+        name: 'factory',
+        position: 0,
+      },
+      {
+        name: 'factory',
+        position: 0,
+      },
+      {
+        name: 'factory',
+        position: 2,
+      },
+      {
+        name: 'factory',
+        position: 2,
+      },
+      {
+        name: 'factory',
+        position: 4,
+      },
+      {
+        name: 'factory',
+        position: 101,
+      },
+      {
+        name: 'factory',
+        position: 101,
+      },
+      {
+        name: 'factory',
+        position: 101,
+      },
+      {
+        name: 'factory',
+        position: 101,
+      },
+      {
+        name: 'factory',
+        position: 4,
+      },
+      {
+        name: 'factory',
+        position: 59,
+      },
+      {
+        name: 'factory',
+        position: 31,
+      },
+      {
+        name: 'factory',
+        position: 42,
+      },
+      {
+        name: 'factory',
+        position: 59,
+      },
+      {
+        name: 'factory',
+        position: 4,
+      },
+      {
+        name: 'factory',
+        position: 101,
+      },
+      {
+        name: 'factory',
+        position: 101,
+      },
+      {
+        name: 'factory',
+        position: 4,
+      },
+      {
+        name: 'factory',
+        position: 4,
+      },
+      {
+        name: 'factory',
+        position: 2,
+      },
+      {
+        name: 'factory',
+        position: 58,
+      },
+      {
+        name: 'factory',
+        position: 31,
+      },
+      {
+        name: 'factory',
+        position: 42,
+      },
+      {
+        name: 'factory',
+        position: 59,
+      },
+      {
+        name: 'factory',
+        position: 58,
+      },
+      {
+        name: 'factory',
+        position: 3,
+      },
+      {
+        name: 'factory',
+        position: 101,
+      },
+      {
+        name: 'factory',
+        position: 2,
+      },
+      {
+        name: 'factory',
+        position: 59,
+      },
+      {
+        name: 'factory',
+        position: 59,
+      },
+      {
+        name: 'factory',
+        position: 59,
+      },
+      {
+        name: 'factory',
+        position: 31,
+      },
+      {
+        name: 'factory',
+        position: 42,
+      },
+      {
+        name: 'factory',
+        position: 59,
+      },
+      {
+        name: 'factory',
+        position: 59,
+      },
+      {
+        name: 'factory',
+        position: 0,
+      },
+      {
+        name: 'factory',
+        position: 101,
+      },
+      {
+        name: 'factory',
+        position: 0,
+      },
+      {
+        name: 'factory',
+        position: 59,
+      },
+      {
+        name: 'factory',
+        position: 58,
+      },
+      {
+        name: 'factory',
+        position: 3,
+      },
+      {
+        name: 'factory',
+        position: 2,
+      },
+      {
+        name: 'factory',
+        position: 3,
+      },
+      {
+        name: 'factory',
+        position: 58,
+      },
+      {
+        name: 'factory',
+        position: 59,
+      },
+      {
+        name: 'factory',
+        position: 0,
+      },
+      {
+        name: 'factory',
+        position: 101,
+      },
+      {
+        name: 'factory',
+        position: 2,
+      },
+      {
+        name: 'factory',
+        position: 3,
+      },
+      {
+        name: 'factory',
+        position: 59,
+      },
+      {
+        name: 'factory',
+        position: 3,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'factory',
+        position: 3,
+      },
+      {
+        name: 'factory',
+        position: 4,
+      },
+      {
+        name: 'factory',
+        position: 59,
+      },
+      {
+        name: 'factory',
+        position: 0,
+      },
+      {
+        name: 'factory',
+        position: 3,
+      },
+      {
+        name: 'factory',
+        position: 101,
+      },
+      {
+        name: 'factory',
+        position: 4,
+      },
+      {
+        name: 'factory',
+        position: 59,
+      },
+      {
+        name: 'factory',
+        position: 3,
+      },
+      {
+        name: 'factory',
+        position: 3,
+      },
+      {
+        name: 'factory',
+        position: 2,
+      },
+      {
+        name: 'factory',
+        position: 58,
+      },
+      {
+        name: 'factory',
+        position: 59,
+      },
+      {
+        name: 'factory',
+        position: 59,
+      },
+      {
+        name: 'factory',
+        position: 2,
+      },
+      {
+        name: 'factory',
+        position: 101,
+      },
+      {
+        name: 'factory',
+        position: 4,
+      },
+      {
+        name: 'factory',
+        position: 58,
+      },
+      {
+        name: 'factory',
+        position: 59,
+      },
+      {
+        name: 'factory',
+        position: 58,
+      },
+      {
+        name: 'factory',
+        position: 59,
+      },
+      {
+        name: 'factory',
+        position: 59,
+      },
+      {
+        name: 'factory',
+        position: 59,
+      },
+      {
+        name: 'factory',
+        position: 58,
+      },
+      {
+        name: 'factory',
+        position: 2,
+      },
+      {
+        name: 'factory',
+        position: 101,
+      },
+      {
+        name: 'factory',
+        position: 101,
+      },
+      {
+        name: 'factory',
+        position: 0,
+      },
+      {
+        name: 'factory',
+        position: 59,
+      },
+      {
+        name: 'factory',
+        position: 59,
+      },
+      {
+        name: 'factory',
+        position: 58,
+      },
+      {
+        name: 'factory',
+        position: 59,
+      },
+      {
+        name: 'factory',
+        position: 0,
+      },
+      {
+        name: 'factory',
+        position: 3,
+      },
+      {
+        name: 'factory',
+        position: 3,
+      },
+      {
+        name: 'factory',
+        position: 101,
+      },
+      {
+        name: 'factory',
+        position: 101,
+      },
+      {
+        name: 'factory',
+        position: 101,
+      },
+      {
+        name: 'factory',
+        position: 0,
+      },
+      {
+        name: 'factory',
+        position: 2,
+      },
+      {
+        name: 'factory',
+        position: 2,
+      },
+      {
+        name: 'factory',
+        position: 0,
+      },
+      {
+        name: 'factory',
+        position: 0,
+      },
+      {
+        name: 'factory',
+        position: 101,
+      },
+      {
+        name: 'factory',
+        position: 101,
+      },
+    ],
+    [
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+      {
+        name: 'empty',
+        position: 0,
+      },
+    ],
+  ],
+  width: 10,
+  height: 10,
+  entities: [
+    {
+      class: 'tunnel-in',
+      position: [6, 1],
+      args: [],
+    },
+    {
+      class: 'tunnel-out',
+      position: [3, 1],
+      args: [],
+    },
+    {
+      class: 'energy-controller',
+      position: [6, 6],
+      args: [],
+    },
+    {
+      class: 'player-spawner',
+      position: [6, 2],
+      args: [],
+    },
+    {
+      class: 'storage',
+      position: [1, 3],
+      args: [],
+    },
+    {
+      class: 'compactor',
+      position: [8, 7],
+      args: [],
+    },
+  ],
+  difficulty: 1,
+};
